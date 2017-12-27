07fd018c13697100b3628246b4a615facf0b0830
AHKerrigan <34887729+AHKerrigan@users.noreply.github.com>
2017-12-27 15:49:06 -0500

    draw_a
    Completed the function for drawing "A" in the letters.py project.

--- letters.py
+++ letters.py
@@ -8,6 +8,23 @@ like vertical and horizontal lines and a few curves. Design an alphabet that can
 be drawn with a minimal number of basic elements and then write functions that
 draw the letters. This uses the turtle function"""
 
+# Primatives 
+def fdbk(t, n):
+	"""Draws a line of length n then returns 
+	"""
+	t.fd(n)
+	t.lt(180)
+	t.fd(n)
+
+def fdbkhalf(t,n):
+	"""Draws a line of length n then returns halfway
+	"""
+	t.fd(n)
+	t.lt(180)
+	t.fd(n / 2)
+
+# GEOMETRIC FUNCTIONS
+
 def polyline(t, n, length, angle):
 	"""Draws n line segments with the given length and
 	angle (in degrees) between them. t is a turtle.
@@ -43,12 +60,33 @@ def circle(t, r):
 	arc(t, r, 360)
 
 
+def hypotenuse(x, y):
+	return (x**2 + y**2)**(1/2)
+
 
 
 # Letters
 # All letters are upper case and n units high
 
-def draw_a()
+def draw_a(t, n):
+	beam_length = hypotenuse(n, n / 2)
+	beam_angle = 90 - math.degrees(math.atan(1/2))
+	cross_length = n - (math.cos(math.radians(beam_angle)) * beam_length)
+	t.lt(beam_angle)
+	t.fd(beam_length)
+	t.rt(beam_angle * 2)
+	fdbkhalf(t, beam_length)
+	t.lt(beam_angle)
+	fdbk(t, cross_length)
+	t.rt(beam_angle)
+	t.fd(beam_length / 2)
+	t.lt(beam_angle)
+
+
+
+"""	
+TODO 
+
 def draw_b()
 def draw_c()
 def draw_d()
@@ -63,7 +101,7 @@ def draw_l()
 def draw_m()
 def draw_n()
 def draw_o(t, n)
-	circle(t, 2n)
+	circle(t, n / 2)
 def draw_p()
 def draw_q()
 def draw_r()
@@ -75,9 +113,10 @@ def draw_w()
 def draw_x()
 def draw_y()
 def draw_z()
+"""
 
 
 
 bob = turtle.Turtle()
-circle(bob, 50)
+draw_a(bob, 300)
 turtle.mainloop()
